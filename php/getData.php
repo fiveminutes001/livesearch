b2954748455b4e1558e4439859ad42b3ba10095a
five minutes <fiveminutes001@gmail.com>
2021-09-13 18:06:02 +0300

    organizing modules

--- php/getData.php
+++ php/getData.php
@@ -5,5 +5,28 @@ $sql="SELECT username FROM playground_demo_all_data WHERE full_name LIKE '".$q."
 $result = mysqli_query($con,$sql);
 echo '<pre>';
 var_dump($sql);
+
+$row = mysqli_fetch_array($result);
+var_dump($row);
+
 var_dump($con);
 echo '</pre>';
+
+// echo "<table>
+// <tr>
+// <th>Firstname</th>
+// <th>Lastname</th>
+// <th>Age</th>
+// <th>Hometown</th>
+// <th>Job</th>
+// </tr>";
+// while($row = mysqli_fetch_array($result)) {
+//   echo "<tr>";
+//   echo "<td>" . $row['FirstName'] . "</td>";
+//   echo "<td>" . $row['LastName'] . "</td>";
+//   echo "<td>" . $row['Age'] . "</td>";
+//   echo "<td>" . $row['Hometown'] . "</td>";
+//   echo "<td>" . $row['Job'] . "</td>";
+//   echo "</tr>";
+// }
+// echo "</table>";
