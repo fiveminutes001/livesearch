7443e1862d962caef6ecb18b489b129ecb731692
five minutes <fiveminutes001@gmail.com>
2021-09-13 17:59:50 +0300

    organizing modules

--- php/getData.php
+++ php/getData.php
@@ -1,7 +1,7 @@
 <?php
 //echo 'q from livesearch.php: '.$q;
 
-$sql="SELECT username FROM playground.demo_all_data WHERE full_name LIKE '".$q."' LIMIT 5";
+$sql="SELECT username FROM playground_demo_all_data WHERE full_name LIKE '".$q."' LIMIT 5";
 $result = mysqli_query($con,$sql);
 echo '<pre>';
 var_dump($con);
