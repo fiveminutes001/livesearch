d8a946c703ef2be41c629eef57c29ca1f7bb8db7
five minutes <fiveminutes001@gmail.com>
2021-09-13 18:01:24 +0300

    organizing modules

--- php/getData.php
+++ php/getData.php
@@ -4,5 +4,6 @@
 $sql="SELECT username FROM playground_demo_all_data WHERE full_name LIKE '".$q."' LIMIT 5";
 $result = mysqli_query($con,$sql);
 echo '<pre>';
+var_dump($sql);
 var_dump($con);
 echo '</pre>';
