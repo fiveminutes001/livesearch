929b4b9d65e7e63a8dda1da260aafe208e7d6f70
five minutes <fiveminutes001@gmail.com>
2021-09-14 15:59:52 +0300

    working on top five results array

--- php/getData.php
+++ php/getData.php
@@ -70,6 +70,7 @@ class query {
 
 function show_top_five_results_array_overall ($top_five_results_array_overall)
 {
+    echo '</pre>';
     echo '<pre>';
     echo '<b>top_five_results_array_overall so far:</b> <br><br>';
     var_dump($top_five_results_array_overall);
